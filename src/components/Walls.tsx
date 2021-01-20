77999e97ffa4b1e7889aa3f16a26fa062aa0a058
Daniel Corner <danielcorner7@gmail.com>
2021-01-20 01:10:48 -0500

    experiment with higher roof

--- src/components/Walls.tsx
+++ src/components/Walls.tsx
@@ -4,72 +4,74 @@ import { Plane } from "./Shapes/Plane";
 // const colors = niceColors[17];
 const colors = ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"];
 
+export const CEILING_HEIGHT_MULTIPLIER = 16;
+
 // https://www.npmjs.com/package/nice-color-palettes
 // https://raw.githubusercontent.com/Jam3/nice-color-palettes/HEAD/visualize/1000.png
 // const palette = niceColors[6]; // e.g. => [ "#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900" ]
 export function Walls() {
   const worldRadius = useStore((state) => state.worldRadius);
   const isRoofOn = useStore((state) => state.isRoofOn);
-  console.log("🌟🚨 ~ Walls ~ isRoofOn", isRoofOn);
-  // const palette = ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"];
+  const ceilingHeight = worldRadius * CEILING_HEIGHT_MULTIPLIER;
   const walls = [
     {
       name: "in front",
+      width: worldRadius * 2,
+      height: worldRadius * 16,
       rotation: [0 * Math.PI, 0, 0],
       color: colors[1],
-      position: [0, -0, -worldRadius],
+      position: [0, ceilingHeight / 2 - worldRadius, -worldRadius],
     },
     {
       name: "behind",
+      width: worldRadius * 2,
+      height: worldRadius * 16,
       rotation: [0, -1 * Math.PI, 0],
       color: colors[1],
-      position: [0, -0, worldRadius],
+      position: [0, ceilingHeight / 2 - worldRadius, worldRadius],
     },
     {
       name: "left",
+      width: worldRadius * 2,
+      height: worldRadius * 16,
       rotation: [0, 0.5 * Math.PI, 0],
       color: colors[1],
-      position: [-worldRadius, 0, 0],
+      position: [-worldRadius, ceilingHeight / 2 - worldRadius, 0],
     },
     {
       name: "right",
+      width: worldRadius * 2,
+      height: worldRadius * 16,
       rotation: [0, -0.5 * Math.PI, 0],
       color: colors[2],
-      position: [worldRadius, -0, 0],
+      position: [worldRadius, ceilingHeight / 2 - worldRadius, 0],
     },
     {
       name: "floor",
+      width: worldRadius * 2,
+      height: worldRadius * 2,
       rotation: [-0.5 * Math.PI, 0, 0],
       color: colors[1],
       position: [0, -worldRadius, 0],
     },
     {
       name: "ceiling",
+      width: worldRadius * 2,
+      height: worldRadius * 2,
       rotation: [0.5 * Math.PI, 0, 0],
       color: colors[1],
-      position: [0, worldRadius, 0],
+      position: [0, ceilingHeight - worldRadius, 0],
     },
   ];
 
-  const width = worldRadius * 2;
-  const height = worldRadius * 2;
-
   return (
     <>
-      {walls.map((props, idx) =>
-        props.name === "ceiling" && !isRoofOn ? null : (
-          <Plane
-            {...props}
-            key={idx}
-            width={width}
-            height={height}
-            // position={
-            //   props.name === "ceiling" && !isRoofOn
-            //     ? [-1000, 0, 0]
-            //     : props.position
-            // }
-          />
+      {walls.map(
+        (props, idx) => (
+          // props.name === "ceiling" && !isRoofOn ? null : (
+          <Plane {...props} key={idx} />
         )
+        // )
       )}
     </>
   );
