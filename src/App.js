aa5c1a8b6aacd884a88f237aefd583e71e63dcd1
dcorn068 <daniel@raventelemetry.com>
2021-01-24 22:20:42 -0500

    enable memorystats

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ import { useStore } from "./store";
 import { LoadingIndicator } from "./components/LoadingIndicator";
 import { useLocalStorageState } from "./utils/useLocalStorageState";
 import { useMount } from "./utils/utils";
+import { render } from "react-dom";
+import MemoryStats from "react-memorystats";
 
 function App() {
-  return (
-    <div className="App">
-      <LoadingIndicator />
-      <LazyLoadedScene />
-      <div id="memoryStats"></div>
-      <Tooltip />
-      {/* <GuidedTour /> */}
-      {/* <SaveControlsSettingsToLocalStorage /> */}
-    </div>
-  );
+	useMount(() => {
+		render(
+			<MemoryStats corner="topLeft" />,
+			document.getElementById("memoryStats")
+		);
+	});
+	return (
+		<div className="App">
+			<LoadingIndicator />
+			<LazyLoadedScene />
+			<div id="memoryStats"></div>
+			<Tooltip />
+			{/* <GuidedTour /> */}
+			{/* <SaveControlsSettingsToLocalStorage /> */}
+		</div>
+	);
 }
 
 export default App;
@@ -26,92 +34,92 @@ export default App;
 const CanvasAndSceneLazy = React.lazy(() => import("./CanvasAndScene"));
 
 function LazyLoadedScene() {
-  const set = useStore((s) => s.set);
-  const started = useStore((s) => s.started);
+	const set = useStore((s) => s.set);
+	const started = useStore((s) => s.started);
 
-  return started ? (
-    <Suspense fallback={null}>
-      <CanvasAndSceneLazy />
-    </Suspense>
-  ) : (
-    <>
-      <CanvasAndSceneEmpty />
-      <div
-        style={{
-          pointerEvents: "none",
-          position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          display: "grid",
-          placeItems: "center",
-          alignContent: "center",
-          gridGap: "1em",
-          minHeight: "100vh",
-        }}
-      >
-        <Typography style={{ textAlign: "center" }} variant="h3">
-          Virus{" "}
-          <span role="img" aria-label="">
-            🦠
-          </span>{" "}
-          Terrarium
-        </Typography>
-        <div
-          style={{
-            display: "grid",
-            gridAutoFlow: "column",
-            placeItems: "center",
-            gridGap: "0.25em",
-          }}
-        >
-          <WarningOutlined />
-          <Typography variant="body2">
-            Requirements: {">"}200MB download, 1GB memory
-          </Typography>
-        </div>
-        <Typography variant="subtitle2">
-          ( this could take a while... )
-        </Typography>
-        <Button
-          style={{ padding: "0.25em 3em", pointerEvents: "auto" }}
-          onClick={() => set({ started: true })}
-          variant="outlined"
-        >
-          Start
-        </Button>
-      </div>
-    </>
-  );
+	return started ? (
+		<Suspense fallback={null}>
+			<CanvasAndSceneLazy />
+		</Suspense>
+	) : (
+		<>
+			<CanvasAndSceneEmpty />
+			<div
+				style={{
+					pointerEvents: "none",
+					position: "fixed",
+					top: 0,
+					left: 0,
+					right: 0,
+					bottom: 0,
+					display: "grid",
+					placeItems: "center",
+					alignContent: "center",
+					gridGap: "1em",
+					minHeight: "100vh",
+				}}
+			>
+				<Typography style={{ textAlign: "center" }} variant="h3">
+					Virus{" "}
+					<span role="img" aria-label="">
+						🦠
+					</span>{" "}
+					Terrarium
+				</Typography>
+				<div
+					style={{
+						display: "grid",
+						gridAutoFlow: "column",
+						placeItems: "center",
+						gridGap: "0.25em",
+					}}
+				>
+					<WarningOutlined />
+					<Typography variant="body2">
+						Requirements: {">"}200MB download, 1GB memory
+					</Typography>
+				</div>
+				<Typography variant="subtitle2">
+					( this could take a while... )
+				</Typography>
+				<Button
+					style={{ padding: "0.25em 3em", pointerEvents: "auto" }}
+					onClick={() => set({ started: true })}
+					variant="outlined"
+				>
+					Start
+				</Button>
+			</div>
+		</>
+	);
 }
 
 function SaveControlsSettingsToLocalStorage() {
-  const set = useStore((s) => s.set);
-  const scale = useStore((s) => s.scale);
-  // const temperature = useStore((s) => s.temperature);
+	const set = useStore((s) => s.set);
+	const scale = useStore((s) => s.scale);
+	// const temperature = useStore((s) => s.temperature);
 
-  const [settings, setSettings] = useLocalStorageState("settings", {
-    // temperature,
-    scale,
-  });
+	const [settings, setSettings] = useLocalStorageState("settings", {
+		// temperature,
+		scale,
+	});
 
-  // when app mounts, retrieve settings from local storage
-  useMount(() => {
-    if (!settings) {
-      return;
-    }
-    // if (settings.temperature) {
-    //   set({ temperature: settings.temperature });
-    // }
-    if (settings.scale) {
-      set({ scale: settings.scale });
-    }
-  });
+	// when app mounts, retrieve settings from local storage
+	useMount(() => {
+		if (!settings) {
+			return;
+		}
+		// if (settings.temperature) {
+		//   set({ temperature: settings.temperature });
+		// }
+		if (settings.scale) {
+			set({ scale: settings.scale });
+		}
+	});
 
-  useEffect(() => {
-    setSettings({ scale });
-  }, [scale, setSettings]);
+	useEffect(() => {
+		setSettings({ scale });
+	}, [scale, setSettings]);
 
-  return null;
+	return null;
 }
