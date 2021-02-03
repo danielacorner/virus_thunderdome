8b6ecd3eb22bbd13f23e7ab50ae099a2999bfd59
Daniel Corner <danielcorner7@gmail.com>
2021-02-03 01:16:53 -0500

    add hp bar at bottom

--- src/components/Walls.tsx
+++ src/components/Walls.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { useStore } from "../store";
+import { PROTEINS } from "../utils/PROTEINS";
 import { Plane } from "./Shapes/Plane";
 import { useScalePercent } from "./useScalePercent";
+import { usePlane } from "@react-three/cannon";
+import { Html } from "@react-three/drei";
+import styled from "styled-components/macro";
+
 // const colors = niceColors[17];
 const colors = ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"];
 
@@ -86,6 +91,87 @@ export function Walls() {
           wireframe={true}
         />
       </mesh>
+      <InteractiveFloorWithHPIndicator
+        {...{
+          name: "floor",
+          width: worldRadius * 2,
+          height: worldRadius * 2,
+          rotation: [-0.5 * Math.PI, 0, 0],
+          // color: colors[1],
+          position: [0, -worldRadius, 0],
+        }}
+      />
     </>
   );
 }
+
+function InteractiveFloorWithHPIndicator({
+  width,
+  height,
+  widthSegments = 1,
+  heightSegments = 1,
+  reflect = false,
+  ...rest
+}) {
+  const initialPlayerHp = 10000
+  const [playerHp, setPlayerHp] = useState(initialPlayerHp)
+  const [ref] = usePlane(() => ({
+    // rotation: [-Math.PI / 2, 0, 0],
+    ...rest,
+    onCollide: (event) => {
+      const { body:collidingBody, target:thisElement } = event as any;
+      // if it's hit by a virus... derease hp
+      const virusCollisionTarget = collidingBody.name&&PROTEINS.viruses.find(v=>v.name===collidingBody.name)
+      if(virusCollisionTarget){
+        setPlayerHp(prev=>prev-virusCollisionTarget.radius)
+      }
+    },
+    // position: [-100, -100, -100],
+  }));
+  const playerHpPct = playerHp / initialPlayerHp
+  return (
+    <mesh ref={ref}>
+      <planeGeometry
+        attach="geometry"
+        args={[width, height, widthSegments, heightSegments]}
+      />
+      <Html>
+        <HPIndicatorStyles {...{playerHpPct}}>
+          <div className="hpBar">
+            <div className="hp"></div>
+          </div>
+        </HPIndicatorStyles>
+      </Html>
+    </mesh>
+  );
+}
+
+const HPIndicatorStyles = styled.div`
+  pointer-events: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 0;
+  .name {
+    font-size: 8px;
+    font-weight: bold;
+    white-space: nowrap;
+    text-shadow: 0px 0px 2px white, 0px 0px 6px white;
+    padding-bottom: 0.5em;
+    text-align: center;
+  }
+  .hpBar {
+    width: 240px;
+    height: 8px;
+    outline: 1px solid grey;
+    .hp {
+      width: 100%;
+      height: 100%;
+      background: limegreen;
+      transition: transform 300ms linear;
+      transform: scaleX(${(p) => p.playerHpPct});
+      transform-origin: left;
+    }
+  }
+`;
