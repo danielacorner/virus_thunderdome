43debdaf6d432dfd174b35a266cc8a5521d1b921
Daniel Corner <danielcorner7@gmail.com>
2021-02-15 06:35:35 -0500

    swap out hiv for more interesting particles

--- src/components/Game/WAVES.tsx
+++ src/components/Game/WAVES.tsx
@@ -9,15 +9,21 @@ import { ReactComponent as Icon5 } from "./icons/virus_5.svg";
 
 export const ICONS = [Icon1, Icon2, Icon3, Icon4, Icon5];
 
-const antibody_hiv = PROTEINS.antibodies.find(
-  (ab) => ab.name === "anti-HIV Antibody"
+// const antibody_hiv = PROTEINS.antibodies.find(
+//   (ab) => ab.name === "anti-HIV Antibody"
+// );
+const antibody_virion = PROTEINS.antibodies.find(
+  (ab) => ab.name === "anti-GTA-Virion Antibody"
+);
+const antibody_bacteriophage_phi29_prohead = PROTEINS.antibodies.find(
+  (ab) => ab.name === "anti-Bacteriophage Antibody"
 );
 const antibody_hpv = PROTEINS.antibodies.find(
   (ab) => ab.name === "anti-HPV Antibody"
 );
-const antibody_herpes = PROTEINS.antibodies.find(
-  (ab) => ab.name === "anti-Herpes Antibody"
-);
+// const antibody_herpes = PROTEINS.antibodies.find(
+//   (ab) => ab.name === "anti-Herpes Antibody"
+// );
 const antibody_poliovirus = PROTEINS.antibodies.find(
   (ab) => ab.name === "anti-Poliovirus Antibody"
 );
@@ -34,13 +40,25 @@ const HPV = {
     (v) => v.name === "Human Papillomavirus (HPV)"
   ),
 };
-const Herpes = {
+// const Herpes = {
+//   iconIdx: 2,
+//   virusData: PROTEINS.viruses.find((v) => v.name === "Herpes"),
+// };
+// const HIV = {
+//   iconIdx: 3,
+//   virusData: PROTEINS.viruses.find((v) => v.name === "HIV"),
+// };
+const GTA_virion = {
   iconIdx: 2,
-  virusData: PROTEINS.viruses.find((v) => v.name === "Herpes"),
+  virusData: PROTEINS.viruses.find(
+    (v) => v.name === "Virion of native gene transfer agent GTA particle"
+  ),
 };
-const HIV = {
+const Bacteriophage_phi29_prohead_80 = {
   iconIdx: 3,
-  virusData: PROTEINS.viruses.find((v) => v.name === "HIV"),
+  virusData: PROTEINS.viruses.find(
+    (v) => v.name === "Bacteriophage phi29 prohead"
+  ),
 };
 const Faustovirus = {
   iconIdx: 4,
@@ -116,13 +134,13 @@ export const WAVES: Wave[] = [
     viruses: [
       { numViruses: 4, virus: Polio },
       { numViruses: 4, virus: HPV },
-      { numViruses: 8, virus: Herpes },
+      { numViruses: 8, virus: GTA_virion },
     ],
-    antibody: antibody_herpes,
+    antibody: antibody_virion,
     assets: [
-      "/models/cells/monocyte.glb",
-      "/models/viruses/hpv_100.glb",
-      "/models/antibodies/antibody_hpv_10.glb",
+      "/models/cells/eosinophil.glb",
+      "/models/viruses/virion_of_native_gene_transfer_agent_gta_particle_120.glb",
+      "/models/antibodies/antibody_herpes.glb",
     ],
     moveCameraTo: [0, 16, 19],
     scaleTarget: 0.003,
@@ -145,13 +163,13 @@ export const WAVES: Wave[] = [
     viruses: [
       { numViruses: 2, virus: Polio },
       { numViruses: 3, virus: HPV },
-      { numViruses: 3, virus: Herpes },
-      { numViruses: 8, virus: HIV },
+      { numViruses: 3, virus: GTA_virion },
+      { numViruses: 6, virus: Bacteriophage_phi29_prohead_80 },
     ],
-    antibody: antibody_hiv,
+    antibody: antibody_bacteriophage_phi29_prohead,
     assets: [
-      "/models/cells/monocyte.glb",
-      "/models/viruses/HIV_200.glb",
+      "/models/cells/basophil.glb",
+      "/models/viruses/bacteriophage_phi29_prohead_80.glb",
       "/models/antibodies/antibody_hiv_10.glb",
     ],
     moveCameraTo: [0, 20, 24],
@@ -173,7 +191,7 @@ export const WAVES: Wave[] = [
   },
   {
     viruses: [
-      { numViruses: 8, virus: HIV },
+      { numViruses: 8, virus: Bacteriophage_phi29_prohead_80 },
       { numViruses: 8, virus: Faustovirus },
     ],
     antibody: antibody_faustovirus,
@@ -187,7 +205,7 @@ export const WAVES: Wave[] = [
     Spring: () => {
       useSpringStoreImmediately({
         property: "ceilingHeight",
-        target: 24,
+        target: 36,
         springConfig: {
           mass: 1,
           tension: 170,
